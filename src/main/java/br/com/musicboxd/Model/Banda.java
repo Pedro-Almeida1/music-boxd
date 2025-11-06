0f761f8eb9e1586f68a66d150325fb4ad2c2b82f
LuisEduardo <luiseduardoribeiro49@gmail.com>
2025-11-05 23:48:19 -0300

    Build: 🏗️ Criação da classe banda

--- src/main/java/br/com/musicboxd/Model/Banda.java
+++ src/main/java/br/com/musicboxd/Model/Banda.java
@@ -1,5 +1,16 @@
 package br.com.musicboxd.Model;
 
 public class Banda {
+    private String nome;
+    //private String integrantes;
+    private String generoMusical;
+    private String paisOrigem; 
 
+
+    public Banda(String nome, String genero, String paisOrigem){
+        this.nome = "nome";
+        //this.integrantes = "cantor1";
+        this.generoMusical = "genero";
+        this.paisOrigem = "país padrão";
+    }
 }
