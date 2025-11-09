129c0b33289a01c471b946ce58886e5af074bd5f
LuisEduardo <luiseduardoribeiro49@gmail.com>
2025-11-09 19:53:51 -0300

    feat✨: Adicionando os métodos da classe Banda

--- src/main/java/br/com/musicboxd/Model/Banda.java
+++ src/main/java/br/com/musicboxd/Model/Banda.java
@@ -1,16 +1,42 @@
 package br.com.musicboxd.Model;
 
+import java.util.ArrayList;
+
 public class Banda {
     private String nome;
-    //private String integrantes;
     private String generoMusical;
-    private String paisOrigem; 
+    private int anoFormacao; 
+    private ArrayList<String> musicas;
 
 
-    public Banda(String nome, String genero, String paisOrigem){
-        this.nome = "nome";
-        //this.integrantes = "cantor1";
-        this.generoMusical = "genero";
-        this.paisOrigem = "país padrão";
+    public Banda(String nome, String genero, int anoFormacao){
+        this.nome = nome;
+        this.generoMusical = genero;
+        this.anoFormacao = anoFormacao;
+        this.musicas = new ArrayList<>();
+    }
+
+    public void AdicionarMusica(String musica){
+        musicas.add(musica);
+    }
+
+    public void removerMusicas(String musica){
+        if(musicas.contains(musica)){
+            musicas.remove(musica);
+            System.out.println("Musica removida com sucesso");
+        }else{
+            System.out.println("A musica não foi encontrada na lista");
+        }
+    }
+
+    public void VerMusicas(){
+        System.out.println("Musicas de " + nome + ":");
+        if(musicas.isEmpty()){
+            System.out.println("Não há músicas para essa banda!");
+        }else{
+            for(String m : musicas){
+                System.out.println("- " + m);
+            }
+        }
     }
 }
