6a140d9b9c2dbc2239393fb00a6f80565fe52021
Marcelo <mdbezerra16@gmail.com>
2025-11-16 14:42:18 -0300

    feat: :sparkles: Salvar Cantor no BD
    - Refatoração da tela de cadastro de cantor

--- src/main/java/br/com/musicboxd/View/TelaCadastroCantor.java
+++ src/main/java/br/com/musicboxd/View/TelaCadastroCantor.java
@@ -7,11 +7,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import br.com.musicboxd.DAO.ArtistaDAO.CantorDAO;
 import br.com.musicboxd.model.Usuario;
+import br.com.musicboxd.model.artistas.Cantor;
 
 public class TelaCadastroCantor {
 
@@ -19,7 +23,7 @@ public class TelaCadastroCantor {
 
 	private JTextField lblNome;
 	private JTextField lblGenero;
-	private JTextField lblAnos;
+	private JTextField lblIdade;
 	private Usuario usuarioLogado;
 
 	/**
@@ -56,50 +60,65 @@ public class TelaCadastroCantor {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Nome:");
-		lblNewLabel.setBounds(74, 70, 43, 14);
-		frame.getContentPane().add(lblNewLabel);
+		JLabel txtNome = new JLabel("Nome:");
+		txtNome.setBounds(74, 70, 43, 14);
+		frame.getContentPane().add(txtNome);
 
 		lblNome = new JTextField();
 		lblNome.setBounds(121, 67, 199, 20);
 		frame.getContentPane().add(lblNome);
 		lblNome.setColumns(10);
 
-		JLabel lblGeneroMusical = new JLabel("Genero Musical:");
-		lblGeneroMusical.setBounds(28, 95, 89, 14);
-		frame.getContentPane().add(lblGeneroMusical);
+		JLabel txtGeneroMusical = new JLabel("Gênero Musical:");
+		txtGeneroMusical.setBounds(28, 95, 89, 14);
+		frame.getContentPane().add(txtGeneroMusical);
 
 		lblGenero = new JTextField();
 		lblGenero.setBounds(121, 95, 199, 20);
 		frame.getContentPane().add(lblGenero);
 		lblGenero.setColumns(10);
 
-		JLabel lblIdade = new JLabel("Idade:");
-		lblIdade.setBounds(72, 120, 45, 14);
-		frame.getContentPane().add(lblIdade);
+		JLabel txtIdade = new JLabel("Idade:");
+		txtIdade.setBounds(72, 120, 45, 14);
+		frame.getContentPane().add(txtIdade);
 
-		lblAnos = new JTextField();
-		lblAnos.setBounds(121, 120, 55, 20);
-		frame.getContentPane().add(lblAnos);
-		lblAnos.setColumns(10);
+		lblIdade = new JTextField();
+		lblIdade.setBounds(121, 120, 55, 20);
+		frame.getContentPane().add(lblIdade);
+		lblIdade.setColumns(10);
 
 		JButton salvarLabel = new JButton("Salvar");
 		salvarLabel.addActionListener(e ->  {
-			String nome = lblNome.getText();
-			String genero = lblGenero.getText();
-			int idade = Integer.parseInt(lblIdade.getText());
+			try {
+				String nome = lblNome.getText();
+				String genero = lblGenero.getText();
+				int idade = Integer.parseInt(lblIdade.getText());
+				
+				if (nome.equals("") || genero.equals("")) {
+					JOptionPane.showMessageDialog(null, "Preencha todos os campos para cadastrar um cantor!",
+							"Erro no cadastro de cantor", JOptionPane.ERROR_MESSAGE);
+				}
+				
+				CantorDAO cantorDAO = new CantorDAO();
+				if (cantorDAO.existePorNome(nome)) {
+					JOptionPane.showMessageDialog(null, "Cantor já cadastrado!",
+							"Erro no cadastro de cantor", JOptionPane.ERROR_MESSAGE);
+				}				
+				
+				Cantor cantor = new Cantor(nome, genero, idade);
+				cantorDAO.salvar(cantor);
+				JOptionPane.showMessageDialog(null, "Cantor cadastrado com sucesso!");
+				limpar();
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(null, "Erro ao cadastrar cantor!",
+						"Erro no cadastro de cantor", JOptionPane.ERROR_MESSAGE);
+			}
 		});
 		salvarLabel.setBounds(112, 151, 89, 23);
 		frame.getContentPane().add(salvarLabel);
 
 		JButton labelLimpar = new JButton("Limpar");
-		labelLimpar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				lblNome.setText("");
-				lblGenero.setText("");
-				lblAnos.setText("");
-			}
-		});
+		labelLimpar.addActionListener(e -> limpar());
 		labelLimpar.setBounds(231, 151, 89, 23);
 		frame.getContentPane().add(labelLimpar);
 
@@ -110,5 +129,11 @@ public class TelaCadastroCantor {
 		});
 		voltarLabel.setBounds(112, 195, 89, 23);
 		frame.getContentPane().add(voltarLabel);
+	}
+	
+	private void limpar() {
+		lblNome.setText("");
+		lblGenero.setText("");
+		lblIdade.setText("");
 	}
 }
